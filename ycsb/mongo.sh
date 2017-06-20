a6e392f9ce5808f3e6f7f5cb56f649bf672b7f01
Alex <palexster@gmail.com>
2017-06-20 10:06:03 +0200

    Updated Benchmark script

--- ycsb/mongo.sh
+++ ycsb/mongo.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 cd /YCSB
-./bin/ycsb load mongodb-async -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s > /tmp/results/workloada
+./bin/ycsb load mongodb-async -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s > /tmp/results/workloada-asynch
+./bin/ycsb run mongodb -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s  > /tmp/results/workloada
+./bin/ycsb load mongodb-async -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s > /tmp/results/workloadb-asynch
 ./bin/ycsb run mongodb -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s  > /tmp/results/workloadb
+./bin/ycsb load mongodb-async -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s > /tmp/results/workloadc-asynch
+./bin/ycsb run mongodb -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s  > /tmp/results/workloadc
+./bin/ycsb load mongodb-async -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s > /tmp/results/workloadd-asynch
+./bin/ycsb run mongodb -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s  > /tmp/results/workloadd
+./bin/ycsb load mongodb-async -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s > /tmp/results/workloade-asynch
+./bin/ycsb run mongodb -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s  > /tmp/results/workloade
+./bin/ycsb load mongodb-async -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s > /tmp/results/workloadf-asynch
+./bin/ycsb run mongodb -P workloads/workloada -p mongodb.url=mongodb://mon1:27017/ycsb?w=0 -s  > /tmp/results/workloadf
 
